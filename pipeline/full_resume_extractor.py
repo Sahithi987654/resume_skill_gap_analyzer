b4bc4d0df4ad5a4f6b88728fe5c66944323f17b5
Sahithi Mandapuri <sahithimandapuri2006@gmail.com>
2026-01-02 18:51:41 +0530

    Replace poppler with pytesseract

--- pipeline/full_resume_extractor.py
+++ pipeline/full_resume_extractor.py
@@ -1,36 +1,20 @@
-import subprocess
 from pathlib import Path
-import shutil
+from PIL import Image
+import pytesseract
+import pdf2image
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-DATA_DIR = BASE_DIR / "data"
 OUTPUT_DIR = BASE_DIR / "outputs"
 OUTPUT_DIR.mkdir(exist_ok=True)
 
-def get_pdftotext_path():
-    
-    system_path = shutil.which("pdftotext")
-    if system_path:
-        return system_path
-
-    
-    local_poppler = BASE_DIR / "pipeline" / "poppler" / "poppler-25.12.0" / "Library" / "bin" / "pdftotext.exe"
-    if local_poppler.exists():
-        return str(local_poppler)
-
-    raise RuntimeError("pdftotext not found. Please install Poppler.")
-
 def extract_text_from_pdf(pdf_path):
+    images = pdf2image.convert_from_path(pdf_path)
+    text = ""
+
+    for img in images:
+        text += pytesseract.image_to_string(img)
+
     output_file = OUTPUT_DIR / "resume_text_final.txt"
-    pdftotext = get_pdftotext_path()
+    output_file.write_text(text, encoding="utf-8")
 
-    subprocess.run(
-        [pdftotext, "-layout", str(pdf_path), str(output_file)],
-        check=True
-    )
-
-    return output_file.read_text(encoding="utf-8", errors="ignore")
-
-if __name__ == "__main__":
-    text = extract_text_from_pdf(DATA_DIR / "Resume.pdf")
-    print("Resume text extracted successfully.")
+    return text
